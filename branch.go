28fb06d27e1c322f073b01feb7ab86b0c37e3bfe
sazima <w@wkadmin.com>
2019-07-17 21:57:41 +0800

    refs #1 test

--- branch.go
+++ branch.go
@@ -5,13 +5,41 @@ import (
 	"io/ioutil"
 )
 
+func grade(score int) string {
+	g := ""
+	switch {
+	case score < 60:
+		g = "f"
+	case score < 70:
+		g = "e"
+	case score < 80:
+		g = "d"
+	case score < 90:
+		g = "c"
+	default:
+		panic(fmt.Sprintf("wrong score %d", score))
+	}
+
+	return g
+}
+
 func main() {
 	const filename = "abc.txt"
-	contents, error := ioutil.ReadFile(filename)
-	if error != nil {
-        fmt.Println(error)
-	}else{
-		fmt.Printf("%s \n", contents)
+	if contents, err := ioutil.ReadFile(filename); err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Printf("%s \n ", contents)
+	}
+	s := grade(11)
+	fmt.Println(s)
+	switch s {
+	case "f":
+		fmt.Printf("terrible")
+	case "a":
+		fmt.Print("great")
+	default:
+		fmt.Printf("I don't konw")
+
 	}
 
 }
